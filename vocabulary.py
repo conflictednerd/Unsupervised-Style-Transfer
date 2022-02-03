21bfdc574cd030b8baf9f56e91a63ffa138cc040
Maryghysr <54866057+Maryghysr@users.noreply.github.com>
2022-02-03 17:19:26 +0330

    modify vocabulary.py

--- vocabulary.py
+++ vocabulary.py
@@ -2,60 +2,58 @@ import csv
 import os
 import torch
 import random 
+import pickle
 import numpy as np
 import pandas as pd
-from bpe import Encoder
-
+# from bpe import Encoder
 
 class Vocab():
-  
+
     # fit BPE
     # tokenize and encode
     # save , load
 
-    def __init__(self):
+    def __init__(self, models_dir, data_path=None, train=None, test=None, dev=None):
         super().__init__()
+        self.MODEL_DIR = models_dir
+        self.TRAIN_DATA_PATH = data_path
+        if train:
+            self.load_train_data(self.TRAIN_DATA_PATH)
+        if test:
+            self.load_test_data(self.TRAIN_DATA_PATH)
+        if dev:
+            self.load_dev_data(self.TRAIN_DATA_PATH)
+
         self.encoder = Encoder(silent=True)
-        self.list_poet = []
-        self.list_poem_tkn = []
-        self.list_poem_bpe = []
-
-    def fit_bpe(self, path_src:str, path_dst:str):
-        with open(path_src) as infile:
-          lines = list(map(str.strip, infile))
-        self.encoder.fit(lines)
-        df = pd.read_csv(path_dst, sep='delimiter')
-        
-        for i in range(len(df)-1) :
-          poem, poet = df.loc[i, 0].split(",")
-          self.list_poem_tkn.append(self.encoder.tokenize(poem))
-          self.list_poem_bpe.append(next(self.encoder.transform([poem])))
-          self.list_poet.append(poet)
-
-        self.list_poem_bpe = self.list_poem_bpe[1:]
-        self.list_poem_tkn = self.list_poem_tkn[1:]
-        self.list_poet = self.list_poet[1:]
     
-    def fit_one_bpe(self, example, tokenize=True, bpe=False):
-      if tokenize and bpe:
-        return (self.encoder.tokenize(example),next(self.encoder.transform([example])))
-      elif bpe:
+    def fit_data(self, path:str):
+        with open(path) as infile:
+          lines = list(map(str.strip, infile))[1:]
+        new_lines = [cmt.split(",")[0] for cmt in lines]
+        self.encoder.fit(new_lines)
+    
+    def tokenize_one(self, example):
+        return (self.encoder.tokenize(example))
+  
+    def encodeing_one(self, example, tokenize=True, bpe=False):
         return next(self.encoder.transform([example]))
-      elif tokenize:
-        self.encoder.tokenize(example)
 
-    def load(self, path: str):
-        df = pd.read_csv(path, sep='delimiter')
-        for i in range(len(df)-1) :
-          poem = df.loc[i, 0].split(",")
-          poem, poet = poem[0], poem[1]
-          self.list_poem_tkn.append(self.encoder.tokenize(poem))
-          self.list_poem_bpe.append(next(self.encoder.transform([poem])))
-          self.list_poet.append(poet)
+    def load_train_data(self, path: str):
+        with open(path + 'train.json', "rb") as f_train:
+          self.train_data = pickle.load(f_train)
+        f_train.close()
 
-    def save(self, path: str):
-        with  open(path, 'w') as myfile:
-          wr = csv.writer(myfile)
-          wr.writerow(["poem_tokenize", "poem_bpe", "poet"])
-        for cmd in range(len(self.list_poem_tkn)):
-          wr.writerow([self.list_poem_tkn[cmd], self.list_poem_bpe[cmd], self.list_poet[cmd]])
+    def load_dev_data(self, path: str):
+        with open(path + 'dev.json', "rb") as f_dev:
+          self.dev_data = pickle.load(f_dev)
+        f_dev.close()
+
+    def load_test_data(self, path: str):
+        with open(path + 'test.json', "rb") as f_test:
+          self.test_data = pickle.load(f_test)
+        f_test.close()
+
+    def save(self, dir_path: str = None):
+        dir_path = dir_path or self.MODEL_DIR
+        with open(dir_path, 'wb') as handle:
+          pickle.dump(self.encoder, handle)
