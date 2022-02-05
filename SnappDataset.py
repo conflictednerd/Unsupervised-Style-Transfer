ef44720189c46b11bfd38d9494510e018e56fc0a
AliMarashian <45737450+AliMarashian@users.noreply.github.com>
2022-02-05 14:23:10 +0330

    Update SnappDataset.py

--- SnappDataset.py
+++ SnappDataset.py
@@ -6,26 +6,22 @@ from tokenizer import Tokenizer
 import torch
 
 
-snp_tokenizer = Tokenizer(name='snp_tokenizer', load=True, type_='snp', models_dir='./models_dir/',
-                          data_file='./data/snappfood/train.csv')
-
-
-def get_snapp_dataset(data_df):
+def get_snapp_dataset(data_df, tokenizer):
     labels = data_df['label'].to_list()
     labels_ = []
 
     for data in labels:
         labels_.append(0 if data == "SAD" else 1)
 
-    df = pd.DataFrame({'text': snp_tokenizer.transform(data_df['comment'].to_list()), 'label': labels_})
+    df = pd.DataFrame({'text': tokenizer.transform(data_df['comment'].to_list()), 'label': labels_})
     hg_dataset = Dataset(pa.Table.from_pandas(df))
     return hg_dataset
 
 
-def create_snapp_dataset_from_path(path, filename):
+def create_snapp_dataset_from_path(path, filename, tokenizer):
     data = pd.read_csv(path + filename)
     print(data.head())
-    return get_snapp_dataset(data)
+    return get_snapp_dataset(data, tokenizer)
 
 
 # def data_collator_snap(batch):
@@ -47,9 +43,15 @@ def create_snapp_dataset_from_path(path, filename):
 #
 #     return text_list.to(device), label_list.to(device),
 
+snp_tokenizer = Tokenizer(name='snp_tokenizer', load=True, type_='snp', models_dir='./models_dir/',
+                          data_file='./data/snappfood/train.csv')
 
-dev_dataset = create_snapp_dataset_from_path("data/snappfood/", "dev.csv")
+dev_dataset = create_snapp_dataset_from_path("data/snappfood/", "dev.csv", snp_tokenizer)
 print(dev_dataset[0])
 print([dev_dataset[0]['text']])
-print(snp_tokenizer.inv_transform([dev_dataset[0]['text']])) ## TODO: why does this not transform the tokens back into their original text?
+print([snp_tokenizer.encoder.bpe_vocab[snp_tokenizer.encoder.EOW]])
+print([snp_tokenizer.encoder.bpe_vocab[
+           66]])  ## It's the first token of that comment, yet it can't find it in either of bpe or word vocabularies
+print(snp_tokenizer.inv_transform(
+    [dev_dataset[0]['text']]))  ## TODO: why does this not transform the tokens back into the original text?
 print(dev_dataset)
